3188a185809f0bcb3c3850244f793608d95f7c79
Rodney Mutembei <50694357+airkongo@users.noreply.github.com>
2022-08-24 12:58:47 +0300

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:([+\w]*)\] \[to:([+\w]*)\] \[flags:(.*?)\]/).join(separator=",")
+ptn = /(?:(?<=from:)(?:\p{L}+|\+?\d+)|(?<=to:)(?:\p{L}+|\+?\d*)|(?<=flags:)(?:-?\d+:?)*)/
+puts ARGV[0].scan(ptn).join(',')
